026bb0393eca46fffbe00c0daa6e685331219243
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-08-13 22:31:26 +0300

    Add project files.

---
+++ ChatAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChatAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
